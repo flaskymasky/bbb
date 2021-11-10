6601cdc6794bd619ba1418a3c53e8bc863c79971
flaskymasky <53376718+flaskymasky@users.noreply.github.com>
2021-11-10 21:24:00 +0100

    Add files via upload

---
+++ AUFGABEN2/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+        int year;
+
+        printf("enter year: ");
+        scanf("%d", &year);
+
+        if ((year % 100 == 0 && year % 400 == 0) ||
+            (year % 4 == 0 && year % 100 != 0))
+                printf("Schaltjahr\n");
+        else
+                printf("kein Schaltjahr\n");
+        
+        return 0;
+}
